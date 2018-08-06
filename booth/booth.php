2c3efdc881eeb9874d88302457c7ab3859e28973
snyabios254 <sydiney14nyabiosi@gmail.com>
2018-08-06 19:09:50 -0400

    #addition of booth

---
+++ booth/booth.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Take a photo</title>
+  <?php
+  require_once("../include/bootstrapLinks.html");
+  include("../include/conn.php");
+  ?>
+</head>
+<body class="jumbotron">
+  <h1 class="alert alert-danger">How are you</h1>
+</body>
+</html>
--- include/conn.php
+++ include/conn.php
@@ -4,16 +4,16 @@ $userName = "root";
 $password = "";
 $conn = '';
 
-$conn1 = mysqli_connect($serverName, $userName, $password);
+$conn = mysqli_connect($serverName, $userName, $password);
 
-if (!$conn1) {
-  echo mysqli_error($conn1);
+if (!$conn) {
+  echo mysqli_error($conn);
 }
 
-$sql = mysqli_query($conn1, "CREATE DATABASE IF NOT EXISTS credoLink;");
+$sql = mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS credoLink;");
 
 if ($sql) {
-  $conn = mysqli_connect($conn, $sql);
+  $conn = mysqli_connect($serverName, $userName, $password, 'credoLink');
 } else {
   echo 'database problem'.mysqli_error($conn);
 }
