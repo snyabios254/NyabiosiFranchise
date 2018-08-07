4c58130955a643ffe9b8a9abe4d85f5a0ea149fb
snyabios254 <sydiney14nyabiosi@gmail.com>
2018-08-07 12:03:35 -0400

    #single page front end complete

---
+++ single/single.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CredoLink Frachise</title>
+<?php
+include("../include/bootstrapLinks.html");
+include("../include/conn.php");
+?>
+</head>
+<body class="jumbotron container">
+<?php include("../include/header.html");?>
+<form method="POST">
+  <label class="badge badge-success">First guarantor information</label>
+  <div class="form-row">
+    <div class="form-group col-lg">
+      <label for="grFirstName">First Name:</label>
+      <input type="text" name="grFirstName" value="" id="grFirstName" class="form-control">
+    </div>
+    <div class="form-group col-lg">
+      <label for="grSecName">Second Name:</label>
+      <input type="text" name="grSecName" value="" id="grSecName" class="form-control">
+    </div>
+    <div class="form-group col-lg">
+      <label for="grThirdName">Third Name:</label>
+      <input type="text" name="grThirdName" value="" id="grThirdName" class="form-control">
+    </div>
+  </div>
+  <div class="form-row">
+    <div class="form-group col-lg">
+      <label for="grIdNo">ID number:</label>
+      <input type="text" name="grIdNo" value="" id="grIdNo" class="form-control">
+    </div>
+    <div class="form-group col-lg">
+      <label for="grPhoneNo">Phone number:</label>
+      <input type="text" name="grPhoneNo" value="" id="grPhoneNo" class="form-control">
+    </div>
+  </div>
+    <label class="badge badge-success">Second guarantor information(optional)</label>
+    <div class="form-row">
+      <div class="form-group col-lg">
+        <label for="gr2FirstName">First Name:</label>
+        <input type="text" name="gr2FirstName" value="" id="gr2FirstName" class="form-control">
+      </div>
+      <div class="form-group col-lg">
+        <label for="gr2SecName">Second Name:</label>
+        <input type="text" name="gr2SecName" value="" id="gr2SecName" class="form-control">
+      </div>
+      <div class="form-group col-lg">
+        <label for="gr2ThirdName">Third Name:</label>
+        <input type="text" name="gr2ThirdName" value="" id="gr2ThirdName" class="form-control">
+      </div>
+    </div>
+    <div class="form-row">
+      <div class="form-group col-lg">
+        <label for="gr2IdNo">ID number:</label>
+        <input type="text" name="gr2IdNo" value="" id="gr2IdNo" class="form-control">
+      </div>
+      <div class="form-group col-lg">
+        <label for="gr2PhoneNo">Phone number:</label>
+        <input type="text" name="gr2PhoneNo" value="" id="gr2PhoneNo" class="form-control">
+      </div>
+    </div>
+  <button type="submit" class="btn btn-outline-dark float-right" name="submit">Next >></button>
+</form>
+</body>
+</html>
