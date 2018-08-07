ac55ec0ebfcb79e0b660d5a483e7dd7b7c22de38
snyabios254 <sydiney14nyabiosi@gmail.com>
2018-08-07 07:38:17 -0400

    #addition of guarantor 2 to front end

--- married.php
+++ married.php
@@ -60,8 +60,41 @@ include("include/header.html");
         <input type="text" name="grPhoneNo" value="" id="grPhoneNo" class="form-control">
       </div>
     </div>
-    <p>Click <a href="#">here</a> to enter guarantor two information(optional).</p>
-    <button type="submit" class="btn btn-outline-dark float-right" name="submit">Next >></button>
+    <div class="form-row">
+      <div class="form-group col-lg">
+        <p>Click <a href="#gr2Info" data-toggle="collapse">here</a> to enter guarantor two information(optional).</p>
+      </div>
+      <div class="form-group col-lg">
+        <button type="submit" class="btn btn-outline-dark float-right" name="submit">Next >></button>
+      </div>
+    </div>
+    <div id="gr2Info" class="collapse">
+      <label class="badge badge-success">Second guarantor information</label>
+      <div class="form-row">
+        <div class="form-group col-lg">
+          <label for="grFirstName">First Name:</label>
+          <input type="text" name="grFirstName" value="" id="grFirstName" class="form-control">
+        </div>
+        <div class="form-group col-lg">
+          <label for="grSecName">Second Name:</label>
+          <input type="text" name="grSecName" value="" id="grSecName" class="form-control">
+        </div>
+        <div class="form-group col-lg">
+          <label for="grThirdName">Third Name:</label>
+          <input type="text" name="grThirdName" value="" id="grThirdName" class="form-control">
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="form-group col-lg">
+          <label for="grIdNo">ID number:</label>
+          <input type="text" name="grIdNo" value="" id="grIdNo" class="form-control">
+        </div>
+        <div class="form-group col-lg">
+          <label for="grPhoneNo">Phone number:</label>
+          <input type="text" name="grPhoneNo" value="" id="grPhoneNo" class="form-control">
+        </div>
+      </div>
+    </div>
   </form>
 </body>
 </html>
